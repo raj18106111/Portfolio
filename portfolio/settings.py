409cb4fd3e9e40c4e373944f264cb64fd11ae0ed
Raj Patel <18106111.ece@hindusthan.net>
2021-08-31 17:27:24 +0530

    adding static root in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,5 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT =  os.path.join(BASE_DIR,"static")
 
 
